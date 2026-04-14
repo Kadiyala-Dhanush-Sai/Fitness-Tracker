6d8af88e9b250f9ae6411e6453c83009695274c1
Kadiyala Dhanush <kadiyaladhanush@gmail.com>
2026-04-14 20:09:20 +0530

    wvwrv

--- server/index.js
+++ server/index.js
@@ -9,7 +9,12 @@ const port = process.env.PORT || 3000;
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://athlletix.netlify.app", // Allow only your frontend
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
 
 // ✅ routes FIRST
 app.get("/", (req, res) => {
